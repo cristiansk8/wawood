593e962a642bb8356ee4c78dded21682c71fb16e
Juan diego <Jzuluaga@seedem.co>
2021-03-08 22:56:02 -0500

    Estilos pagina portafolio

--- sites/all/themes/wawood/templates/views/views-view-unformatted--portfolio_new.tpl.php
+++ sites/all/themes/wawood/templates/views/views-view-unformatted--portfolio_new.tpl.php
@@ -1,7 +1,7 @@
 <div class="briefcase">
     <div class="briefcase__tabs">
         <?php foreach ($view->result as $delta => $row) : ?>
-            <div id="<?php echo $delta ?>" class="briefcase__tab-<?php echo $delta ?>">
+            <div id="<?php echo $delta ?>" class="tab briefcase__tab-<?php echo $delta ?>">
                 <?php
                 $lenguage = $row->_field_data['nid']['entity']->language;
                 $title = $row->_field_data['nid']['entity']->title_field[$lenguage][0]['value'];
@@ -15,18 +15,32 @@
         <?php endforeach ?>
     </div>
     <div class="briefcase__content">
+        <?php $array = [] ?>
         <?php foreach ($view->result as $delta => $row) : ?>
             <div class="briefcase__content-<?php echo $delta ?>" data-id="<?php echo $delta ?>">
                 <?php
                 $lenguage = $row->_field_data['nid']['entity']->language;
                 $title = $row->_field_data['nid']['entity']->title_field[$lenguage][0]['value'];
                 $body = $row->_field_data['nid']['entity']->body[$lenguage][0]['value'];
+                isset($row->_field_data['nid']['entity']->field_images['und'])
+                    ? $images = $row->_field_data['nid']['entity']->field_images['und']
+                    : $images = '';
                 ?>
                 <div class="briefcase__content-title">
                     <h3><?php echo $title  ?></h3>
                 </div>
-                <div class="briefcase__body">
+                <div class="briefcase__content-body">
                     <p><?php echo $body ?></p>
+                </div>
+                <div class="briefcase__content-images">
+                    <?php
+                    if (!empty($images)) {
+                        foreach ($images as $image) {
+                            $img_url = file_create_url($image['uri']) ?? '';
+                            print '<div class="briefcase__content-image"><img src="' . $img_url . '"></div>';
+                        }
+                    }
+                    ?>
                 </div>
             </div>
         <?php endforeach ?>
