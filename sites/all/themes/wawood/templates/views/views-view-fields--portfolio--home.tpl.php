c1cc91e1fa559df36e3f1c4baede7d68548dc2e6
Juan diego <Jzuluaga@seedem.co>
2021-03-22 18:11:19 -0500

    Correccion ruta portafolio home

--- sites/all/themes/wawood/templates/views/views-view-fields--portfolio--home.tpl.php
+++ sites/all/themes/wawood/templates/views/views-view-fields--portfolio--home.tpl.php
@@ -1,9 +1,11 @@
 <?php 
-$options = array('absolute' => TRUE);
-$url_node = url('node/' . $row->nid, $options);
+global $language ;
+$base_url = $GLOBALS['base_url'];
+$lang_name = $language->language ;
+$url = "$base_url/$lang_name/portfolio/$row->nid";
 ?>
 <div class="work work-item" data-animation="moveLeft" data-nid="<?php print $row->nid ?>">
-  <a href="<?php print $url_node ?>">
+  <a href="<?php print $url ?>">
     <?php print $fields['field_thumbnail']->content ?>
     <div class="mask">
       <div class="label"><?php print $fields['field_headline']->content ?></div>
