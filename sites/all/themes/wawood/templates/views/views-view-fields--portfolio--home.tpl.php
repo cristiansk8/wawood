b706f43144ce325c2744bf3f8817b581a965b651
Juan diego <Jzuluaga@seedem.co>
2021-03-22 17:39:23 -0500

    Se agrega enlace portafolio home

--- sites/all/themes/wawood/templates/views/views-view-fields--portfolio--home.tpl.php
+++ sites/all/themes/wawood/templates/views/views-view-fields--portfolio--home.tpl.php
@@ -1,6 +1,12 @@
+<?php 
+$options = array('absolute' => TRUE);
+$url_node = url('node/' . $row->nid, $options);
+?>
 <div class="work work-item" data-animation="moveLeft" data-nid="<?php print $row->nid ?>">
-  <?php print $fields['field_thumbnail']->content ?>
-  <div class="mask">
-    <div class="label"><?php print $fields['field_headline']->content ?></div>
-  </div>
+  <a href="<?php print $url_node ?>">
+    <?php print $fields['field_thumbnail']->content ?>
+    <div class="mask">
+      <div class="label"><?php print $fields['field_headline']->content ?></div>
+    </div>
+  </a>
 </div>
