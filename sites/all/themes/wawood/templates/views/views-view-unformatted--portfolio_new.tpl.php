d6cce73752041a6db80453ad1324f8931b30625a
Juan diego <Jzuluaga@seedem.co>
2021-03-22 19:05:42 -0500

    Se edita template new portafolio

--- sites/all/themes/wawood/templates/views/views-view-unformatted--portfolio_new.tpl.php
+++ sites/all/themes/wawood/templates/views/views-view-unformatted--portfolio_new.tpl.php
@@ -3,9 +3,10 @@
         <?php foreach ($view->result as $delta => $row) : ?>
             <div id="<?php echo $delta ?>" class="tab briefcase__tab-<?php echo $delta ?>  <?php echo $delta == 0 ? 'active' : '' ?>">
                 <?php
-                $lenguage = $row->_field_data['nid']['entity']->language;
-                $title = $row->_field_data['nid']['entity']->title_field[$lenguage][0]['value'];
-                $image_url = file_create_url($row->_field_data['nid']['entity']->field_image['und'][0]['uri']);
+                global $language;
+                $lang_name = $language->language ;
+                $title = $row->node_title ;
+                $image_url = file_create_url($row->field_field_image[0]['raw']['uri']);
                 ?>
                 <?php print '<img class="briefcase__image" src="' . $image_url . '">' ?>
                 <div class="briefcase__title">
@@ -19,11 +20,10 @@
         <?php foreach ($view->result as $delta => $row) : ?>
             <div class="content briefcase__content-<?php echo $delta ?> <?php echo $delta == 0 ? 'active' : '' ?>"  data-id="<?php echo $delta ?>">
                 <?php
-                $lenguage = $row->_field_data['nid']['entity']->language;
-                $title = $row->_field_data['nid']['entity']->title_field[$lenguage][0]['value'];
-                $body = $row->_field_data['nid']['entity']->body[$lenguage][0]['value'];
-                isset($row->_field_data['nid']['entity']->field_images['und'])
-                    ? $images = $row->_field_data['nid']['entity']->field_images['und']
+                $title = $row->node_title;
+                $body = $row->field_body[0]['raw']['value'];
+                isset($row->field_field_images)
+                    ? $images = $row->field_field_images
                     : $images = '';
                 ?>
                 <div class="briefcase__content-title">
